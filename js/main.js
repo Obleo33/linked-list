84ce37ed4b81fdb6df738adf4dc520237a7c3b11
Franklin Crosby <obleo33@gmail.com>
2016-12-12 18:44:27 -0700

    Delete button fuctionality enabled

--- js/main.js
+++ js/main.js
@@ -4,14 +4,26 @@ function makeLinkCard (title,url){
   $("#site-list").append(
     '<div class = "site-card">' +
       '<h2 class="site-title">'+ title + '</h2>'+
+      '<div class = "section-break"></div>' +
       '<p class = "site-url">'+ url + '</p>' +
+      '<div class = "section-break"></div>' +
       '<button class = "read-button">Read</button>' +
-      '<button class = "delete-button">Delete</button>'
+      '<button class = "delete-button">Delete</button>' +
+    '</div>'
   );
 }
 
 $('#enter').on('click', function () {
   var $siteTitleValue = $('.site-title-input').val();
   var $siteUrlValue = $('.site-url-input').val();
-  makeLinkCard($siteTitleValue,$siteUrlValue);
+  console.log ($siteUrlValue)
+  makeLinkCard($siteTitleValue, $siteUrlValue);
 });
+
+$('#site-list').on('click', '.delete-button',removeCard);
+
+
+function removeCard(){
+  console.log("putput");
+  $(this).parent().remove();
+};
