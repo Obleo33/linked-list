b79177da32c4d83a236f513d80d50a7b4690bc2a
Franklin Crosby <obleo33@gmail.com>
2016-12-12 17:49:45 -0700

    Add card functionallity populating site list

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
-function getValues (){
-  var $siteTitleValue = $('.site-title-input').val();
-  var $siteUrlValue = $('.site-url-input').val();
-}
 
-function makeLinkCard (){
-  $(".listed-sites").append('<h2 class="headding">heynow</h2>');
+
+function makeLinkCard (title,url){
+  $("#site-list").append(
+    '<div class = "site-card">' +
+      '<h2 class="site-title">'+ title + '</h2>'+
+      '<p class = "site-url">'+ url + '</p>' +
+      '<button class = "read-button">Read</button>' +
+      '<button class = "delete-button">Delete</button>'
+  );
 }
 
 $('#enter').on('click', function () {
-  getValues();
-  makeLinkCard();
+  var $siteTitleValue = $('.site-title-input').val();
+  var $siteUrlValue = $('.site-url-input').val();
+  makeLinkCard($siteTitleValue,$siteUrlValue);
 });
